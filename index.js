f308c4187ac3c28fe141e2ea4741b1e2471f4f6a
Tangang Mc Bride Tabufor <tangangbride@gmail.com>
2023-03-21 22:27:09 +0100

    Added minutes and hours

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 function time() {
   let sec = document.querySelector(".sec");
-  let min = document.querySelector('.min');
+  let min = document.querySelector(".min");
+  let hrs = document.querySelector(".hrs");
+  let amPm = document.querySelector('.AMPM');
+  let am = 'AM';
   let date = new Date();
+  
+ 
+
   let minutes = date.getMinutes();
   let hours = date.getHours();
   let seconds = date.getSeconds();
 
+
+  if(hrs > 12){
+    hrs -= 12;
+    am = 'PM'
+
+  }
+  hrs.innerHTML = hours;
   min.innerHTML = minutes;
   sec.innerHTML = seconds;
+  amPm.innerHTML = am;
+
 }
 
 let interval = setInterval(time, 1000);
