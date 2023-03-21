3c0b3bf0568ea40a0755df21b8c40deefb7cb7e8
Tangang Mc Bride Tabufor <tangangbride@gmail.com>
2023-03-21 21:19:06 +0100

    Implemented dynamic minutes

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 function time() {
   let sec = document.querySelector(".sec");
+  let min = document.querySelector('.min');
   let date = new Date();
+  let minutes = date.getMinutes();
   let hours = date.getHours();
   let seconds = date.getSeconds();
+
+  min.innerHTML = minutes;
   sec.innerHTML = seconds;
 }
 
